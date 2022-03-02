83ca46951334095e8968f1db852bc16c1e50ab7a
Yovie Alfa Guistuta <yoviealfaguistuta@gmail.com>
2022-03-02 20:25:47 +0700

    Disable Postgrees .env Load

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -3,20 +3,18 @@ package controllers
 import (
 	"database/sql"
 	"log"
-	"os"
 
 	"github.com/gofiber/fiber/v2"
-	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
 
 func createConnection() *sql.DB {
-	err := godotenv.Load(".env")
-	if err != nil {
-		// log.Fatalf("Error loading .env file")
-	}
+	// err := godotenv.Load(".env")
+	// if err != nil {
+	// 	// log.Fatalf("Error loading .env file")
+	// }
 
-	db, err := sql.Open("postgres", os.Getenv("POSTGRES_URL"))
+	db, err := sql.Open("postgres", "user=postgres password=localhost dbname=portfolio sslmode=disable")
 	if err != nil {
 		log.Fatal(err)
 	}
