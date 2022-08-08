49bcf91236fc28b1a79e30da7075493fecab4544
Yovie Alfa Guistuta <yoviealfaguistuta@gmail.com>
2022-08-08 15:25:39 +0700

    Fixing migration seeder

--- internal/models/portofolio-list.go
+++ internal/models/portofolio-list.go
@@ -8,7 +8,7 @@ type PortofolioList struct {
 	ID           int         `json:"id" form:"id" xml:"id" example:"1"`
 	Title        string      `json:"title" form:"title" xml:"title" example:"IPKD"`
 	Descriptions string      `json:"descriptions" form:"descriptions" xml:"descriptions" example:"Indeks Pengelolaan Keuangan Daerah"`
-	CreatedAt    interface{} `json:"updated_at" form:"updated_at" xml:"created_at"`
+	CreatedAt    interface{} `json:"created_at" form:"created_at" xml:"created_at"`
 	Images       string      `json:"images" form:"images" xml:"images" example:"/assets/*png"`
 }
 
